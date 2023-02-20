aa7976d34a2477d373a21e4e4bfc08c6dc765717
Abu Naimzadeh <naimzaa8@gmail.com>
2023-02-19 19:51:36 -0500

    Update streamlit_app.py
    Changed text styling to header and displayed full queried data

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,9 @@ streamlit.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The Fruit List Contains:")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The Fruit List Contains:")
+streamlit.dataframe(my_data_rows)
 
 
 
