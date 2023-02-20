f9f2b12c0050b413429013bf6b4979c1a6541cfa
Abu Naimzadeh <naimzaa8@gmail.com>
2023-02-19 19:50:10 -0500

    Update streamlit_app.py
    #errorfix typo in select statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ streamlit.dataframe(fruityvice_normalized)
 # connecting streamlit app and snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list)")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The Fruit List Contains:")
 streamlit.text(my_data_row)
